4798dd67f1b0f8dbb9468d7a2a5a6d0f6ad42e5c
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2021-07-30 03:09:27 -0300

    Criando e estruturando o menu

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import tkinter
+from typing import Text
 
+#  Creating window
 window = tkinter.Tk()
-window.title("Notepad")
-window.geometry("640x480")
-window.minsize(width=640, height=480)
+window.title("Notepad") #  setting the window name
+window.geometry("640x480")  #  setting size of window
+window.minsize(width=640, height=480)  #  Setting minimum size
+#  setting text area
 text_area = tkinter.Text(window, font="Arial 15 bold", width=1280, height=720)
 text_area.pack()
-
-main_menu = tkinter.Menu(window)
-
+#  creating menu and commands
+main_menu = tkinter.Menu(window)  #  creating menu
 file_menu = tkinter.Menu(main_menu, tearoff=0)
 file_menu.add_command(label="New", command=None)
 file_menu.add_command(label="Save", command=None)
 file_menu.add_command(label="Save as...", command=None)
 file_menu.add_command(label="Exit", command=window.quit)
-
 main_menu.add_cascade(label="File", menu=file_menu)
-
 window.config(menu=main_menu)
+
 window.mainloop()
